4f2789eff5a81aa6ed84edff670213c541f11b01
larryinx <84784670+larryinx@users.noreply.github.com>
2025-02-15 06:57:36 +0000

    Deploying to gh-pages from @ larryinx/larryinx.github.io@05dae654b7fda0880647d770aa69f577379f1d34 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-hello-my-personal-website-let-s-make-a-brithday-for-it-glad-to-see-you-there-but-it-was-still-under-construction-hopefully-it-will-be-done-soon",
+          title: 'Hello my personal website! Let’s make a brithday for it😍. Glad to see...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
