dd6490c9b8ccc93e064ef583db2bf60dec94bb9a
larryinx <84784670+larryinx@users.noreply.github.com>
2025-10-17 04:42:41 +0000

    Deploying to gh-pages from @ larryinx/larryinx.github.io@9427b98407ca1e45eae738cb342f76636817ffb2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "";
               },
-            },{id: "dropdown-publications-under-construction",
-              title: "publications (under construction)",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
             },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
