0a5a11037af1262f4809433998f817161976a508
larryinx <84784670+larryinx@users.noreply.github.com>
2025-10-17 21:12:36 +0000

    Deploying to gh-pages from @ larryinx/larryinx.github.io@adb000414e9caae7a80d5226c4e02897955c7ccf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -474,6 +474,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/yinxi-li-071089233", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/LarryYinxiLi", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
